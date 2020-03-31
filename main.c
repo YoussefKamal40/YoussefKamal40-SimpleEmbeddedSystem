b5eae28e8d193033091f0f13efacec01d62c0841
sarah-abdelrahman <saraabdelrahman967@gmail.com>
2020-03-31 15:28:14 +0200

    update switch and LCD

--- main.c
+++ main.c
@@ -1,11 +1,10 @@
 #include "STD_TYPES_H.h"
 #include "RCC_interface.h"
-#include "HSWITCH_interface.h"
-
+#include "SwitchHandler_interface.h"
 void main(void)
 {
-	RCC_u8_ControlPrephiral(RCC_u32_GIOPA_PERIP,ENABLE);
 	HSWITCH_init();
+//	RCC_u8_ControlPrephiral(RCC_u32_CONTROL_PERIP_IOPAEN,1);
   while (1)
     {
        // Add your code here.
