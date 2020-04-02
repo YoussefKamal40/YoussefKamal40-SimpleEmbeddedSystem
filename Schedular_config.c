c12a49bd6688d2e5a9feb7516b59e780c4563e1d
YoussefKamal40 <youx199@gmail.com>
2020-04-02 18:44:04 +0200

    Updating schedular

--- Schedular_config.c
+++ Schedular_config.c
@@ -8,13 +8,15 @@
 #include "Schedular_interface.h"
 #include "Schedular_config.h"
 
-//extern void func (void);
-//extern void func2 (void);
+extern void LCD_runnable(void);
+void HSWITCH_debounce_runnable(void);
+void counterApp_runnable(void);
 
 const taskConfig_t sysTaskConfig[NUMBER_OF_TASKS]=
 {
-	//{.taskRunnable=&func,.periodicity_us=5000,.firstDelayInTick=5},
-	//{.taskRunnable=&func2,.periodicity_us=5000,.firstDelayInTick=0}
+	{.taskRunnable=&HSWITCH_debounce_runnable,.periodicity_us=5000,.firstDelayInTick=15},
+	{.taskRunnable=&LCD_runnable,.periodicity_us=5000,.firstDelayInTick=0},
+	{.taskRunnable=&counterApp_runnable,.periodicity_us=5000,.firstDelayInTick=20}
 };
 
 
